07a67a6a48f1a35ddf256ed2899bb0e039e90410
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-09-06 13:55:35 +0200

    ffff

--- src/utilidadesString.java
+++ src/utilidadesString.java
@@ -1,14 +1,37 @@
 
 public class utilidadesString {
 	/**
-	 * cuenta las plabras que esten contenidas dentro del texto
-	 * @param texto string donde buscar las palabras
-	 * @param busqueda palabra que buscamos dentro del texto
+	 * cuenta las palabras que esten contenidas dentro del texto
+	 * 
+	 * @param texto
+	 *            string donde buscar las palabras
+	 * @param busqueda
+	 *            palabra que buscamos dentro del texto
 	 * @return int 0 si no encuentra, numero de palabras encontradas
 	 */
-	static int buscarPalabra( String texto, String busqueda) {
-		int resultado=0;
+	static int buscarPalabra(String texto, String busqueda) {
+		int resultado = -1;
 		
+		if (texto == null || busqueda ==null) {
+			resultado = 0;
+		}else {
+						
+			int pos = texto.indexOf(busqueda);
+			//while o for hasta pos =-1
+			
+			do {
+				//busca la palabra
+				texto = texto.substring(pos)+1;
+				//esiste???
+				if (pos != -1) {
+					resultado++;
+					pos = texto.indexOf(busqueda);
+				}
+				
+				
+				
+			} while (pos !=-1);					
+		}
 		
 		return resultado;
 	}
--- src/utilidadesStringtest.java
+++ src/utilidadesStringtest.java
@@ -3,18 +3,19 @@ import junit.framework.TestCase;
 public class utilidadesStringtest extends TestCase {
 
 	public void testBuscarPalabra() {
+
+		// casos null
 		
-		//caso facil
-		assertEquals(1, utilidadesString.buscarPalabra("Ahora toca descanso", "descanso") );
-		assertEquals(2, utilidadesString.buscarPalabra("Ahora toca descanso descanso", "descanso") );
-		assertEquals(0, utilidadesString.buscarPalabra("Ahora toca descansodescanso", "descanso") );
+		assertEquals(0, utilidadesString.buscarPalabra(null, null));
+		assertEquals(0, utilidadesString.buscarPalabra(null, "descanso"));
+		assertEquals(0, utilidadesString.buscarPalabra("Ahora toca descanso", null));
 		
-		//casos null
-		assertEquals(0, utilidadesString.buscarPalabra(null, null) );
-		assertEquals(0, utilidadesString.buscarPalabra(null, "descanso") );
-		assertEquals(0, utilidadesString.buscarPalabra("Ahora toca descanso", null) );
-		
-		
+		// caso facil
+		assertEquals(1, utilidadesString.buscarPalabra("Ahora toca descanso", "descanso"));
+		assertEquals(1, utilidadesString.buscarPalabra("Ahora toca descanso.", "descanso"));
+		assertEquals(2, utilidadesString.buscarPalabra("Ahora toca descanso descanso", "descanso"));
+		assertEquals(0, utilidadesString.buscarPalabra("Ahora toca descansodescanso", "descanso"));
+
 	}
 
 }
