fd4681e32c10ffb84afd01a5aca49fb8627625ed
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-09-06 12:43:00 +0200

    dd

---
+++ src/utilidadesString.java
@@ -0,0 +1,16 @@
+
+public class utilidadesString {
+	/**
+	 * cuenta las plabras que esten contenidas dentro del texto
+	 * @param texto string donde buscar las palabras
+	 * @param busqueda palabra que buscamos dentro del texto
+	 * @return int 0 si no encuentra, numero de palabras encontradas
+	 */
+	static int buscarPalabra( String texto, String busqueda) {
+		int resultado=0;
+		
+		
+		return resultado;
+	}
+
+}
