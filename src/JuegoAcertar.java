f4a548ec974da9d6d3f2514f3b3d29c4fe74507f
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-09-07 10:55:31 +0200

    se sale del bucle sin acertar

--- src/JuegoAcertar.java
+++ src/JuegoAcertar.java
@@ -4,33 +4,34 @@ public class JuegoAcertar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int numeroAleatorio = 2;
+		int numeroAleatorio = (int) (Math.random() * 10);
 		int intro = 0;
 		int contador = 0;
 		Scanner pedir = new Scanner(System.in);
-		boolean acertar = false;
-		
+		boolean acertar1 = false;
+
 		System.out.println("introduzca el numero entero que quieres adivinar");
 		intro = pedir.nextInt();
-		
+
 		do {
 			if (numeroAleatorio == intro) {
-			System.out.println("eres un figura acertaste");
-			acertar = true;
-			break;
-		}else {
-			
-			System.out.println("eres un manta no acertaste");
-			//pedir = new Scanner(System.in);
-			intro = pedir.nextInt();
-			contador++;
-			if(contador>=2) {
-				System.out.println("GAME OVER");
+				System.out.println("eres un figura acertaste");
+				acertar1 = true;
+
+				// break;
+			} else {
+
+				System.out.println("eres un manta no acertaste");
+				pedir = new Scanner(System.in);
+				intro = pedir.nextInt();
+				contador++;
+				if (contador >= 3) {
+					System.out.println("GAME OVER");
+				}
+
 			}
-			
-		}
-		} while ((contador<2) && (acertar = false));
-		
+		} while ((contador <= 3) && (acertar1 = false));
+
 		pedir.close();
 	}
 
