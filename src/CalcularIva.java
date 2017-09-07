5276520f42c1ead2651d9e3e61040ff0b2e4b022
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-09-07 09:04:08 +0200

    ultimo

--- src/CalcularIva.java
+++ src/CalcularIva.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class CalcularIva {
 
 	public static void main(String[] args) {
-		System.out.println("   calculadora iva 21%   ");
+		Scanner sc = new Scanner(System.in);
+		String validar = "si";
+		do {
+			System.out.println("   calculadora iva 21%   ");
 		System.out.println("----------------------------------");
 		System.out.println("por favor dale el importe en euros");
-		Scanner sc = new Scanner(System.in);
+		
 		
 		float importe = sc.nextFloat();
-		
 		System.out.println("iva repercutido="+importe*0.21);
+		System.out.println("desea volver a introducir un valor??");
+		sc.next();
 		
-
+		} while (sc.next().equalsIgnoreCase(validar) == true);
 		
 
 
