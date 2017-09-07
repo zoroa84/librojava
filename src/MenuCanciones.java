2eadfbd225fa4e3e0453b9b5086a2fea292a7c9f
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-09-07 13:31:00 +0200

    empezando la aplicacion

---
+++ src/MenuCanciones.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class MenuCanciones {
+
+	ArrayList song = new ArrayList();
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("MENU CANCIONES");
+		System.out.println("elija 1 para listar, 2 oara añadir y 3 para eliminar");
+		
+
+	}
+
+}
