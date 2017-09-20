cac241b1b602ff9a2d2981180eda89d13d7ec2ca
Administrador <Administrador@Formacion-TOSH9.ipartekaula.com>
2017-09-20 14:18:59 +0200

    no se que error es mirar

--- src/com/ipartek/formacion/javalibro/pojo/Persona.java
+++ src/com/ipartek/formacion/javalibro/pojo/Persona.java
@@ -1,93 +1,130 @@
 package com.ipartek.formacion.javalibro.pojo;
+import com.ipartek.formacion.javalibro.excepciones.PersonaException;
+import com.ipartek.formacion.javalibro.utilidades.Validaciones;
 
 public class Persona {
-
+	
+	public static int MAYOR_EDAD = 18;
+	
+	
 	private String nombre;
 	private String apellido1;
 	private String apellido2;
-	private String edad;
 	private String email;
 	private String dni;
-	private String puesto;
-
-	//constructor	
+	private String rol;
+	private int edad;
 	
-	public Persona(String nombre, String apellido1, String apellido2, String edad, String email, String puesto,
-			String dni) {
+	
+	public Persona(String nombre, String apellido1, String apellido2, String email, String dni, String rol, String edad) throws PersonaException {
 		super();
 		this.nombre = nombre;
 		this.apellido1 = apellido1;
 		this.apellido2 = apellido2;
-		this.edad = edad;
 		this.email = email;
 		this.dni = dni;
-		this.puesto = puesto;
+		this.rol = rol;
+		//this.edad = edad;
+		 Integer.toString(setEdad(edad));
 	}
-	
-	//getter y seters
-	
+
+
 	public String getNombre() {
 		return nombre;
 	}
+
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+
 	public String getApellido1() {
 		return apellido1;
 	}
+
 
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
 
+
 	public String getApellido2() {
 		return apellido2;
 	}
+
 
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
 
-	public String getEdad() {
-		return edad;
-	}
-
-	public void setEdad(String edad) {
-		this.edad = edad;
-	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
+
+	public void setEmail(String email) throws PersonaException {
+		
+		if ( !Validaciones.email(email) ) {
+			throw new PersonaException(PersonaException.MSG_EMAIL_ERROR);
+		}		
 		this.email = email;
 	}
 
-	public String getPuesto() {
-		return puesto;
-	}
-
-	public void setPuesto(String puesto) {
-		this.puesto = puesto;
-	}
 
 	public String getDni() {
 		return dni;
 	}
 
+
 	public void setDni(String dni) {
+		if ( !Validaciones.dni(dni) ) {
+			try {
+				throw new PersonaException(PersonaException.MSG_DNI_ERROR);
+			} catch (PersonaException e) {
+				e.printStackTrace();
+			}
+		}	
 		this.dni = dni;
 	}
 
-	//to string
-	
+
+	public String getRol() {
+		return rol;
+	}
+
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+    /**
+     * Setea la edad de la Persona
+     * @param edad int años de la Persona
+     * @throws PersonaException edad < 18
+     */
+	public void setEdad(int edad) throws PersonaException {
+		if ( edad < MAYOR_EDAD ) {
+			throw new PersonaException(PersonaException.MSG_EDAD_MENOR);
+		} 
+		this.edad = edad;
+	}
+
+
 	@Override
 	public String toString() {
-		return "Persona [nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", edad=" + edad
-				+ ", email=" + email +  ", dni=" + dni + ", puesto=" + puesto +"]";
+		return "Persona [nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", email="
+				+ email + ", dni=" + dni + ", rol=" + rol + ", edad=" + edad + "]";
 	}
+	
+	
+	
+	
 
 }
